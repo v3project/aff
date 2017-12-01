c7f963b42d969a402ac6b3585f46e56268adb719
semenov <semenov@skeeks.com>
2017-12-01 19:04:34 +0300

    init

---
+++ src/config/console.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link https://skeeks.com/
+ * @copyright (c) 2010 SkeekS
+ * @date 06.11.2017
+ */
+$config = [
+    'components'    =>
+    [
+        
+    ],
+];
+
+
+return $config;
